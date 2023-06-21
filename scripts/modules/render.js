4e4d06e4d24080bee89733c45abd8bc183b25cd5
Elena Bukina <selenaastu@gmail.com>
2023-06-21 11:41:16 +0400

    update render and utils

--- scripts/modules/render.js
+++ scripts/modules/render.js
@@ -2,7 +2,7 @@ import { getCurrentDateTime } from "./utils.js";
 
 
 const renderWidgetToday = (widget) => {
-    const currentDateTime = getCurrentDateTime();
+    const {dayOfMonth, month, year, dayOfWeek, hours, minutes} = getCurrentDateTime();
 
 
     widget.insertAdjacentHTML(
@@ -10,9 +10,9 @@ const renderWidgetToday = (widget) => {
         `
         <div class="widget__today">
             <div class="widget__date-block">
-                <p class="widget__date">${currentDateTime.dayOfMonth} ${currentDateTime.month} ${currentDateTime.year}</p>
-                <p class="widget__time">${currentDateTime.hours}:${currentDateTime.minutes}</p>
-                <p class="widget__day">${currentDateTime.dayOfWeek}</p>
+                <p class="widget__date">${dayOfMonth} ${month} ${year}</p>
+                <p class="widget__time">${hours}:${minutes}</p>
+                <p class="widget__day">${dayOfWeek}</p>
         </div>
         <div class="widget__icon">
             <img class="widget__img" src="./icon/01d.svg" alt="Погода">
